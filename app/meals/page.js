cba13e8b6f4ba93aa6ef7d58a115034104c9599b
Kapil Jugnu <kr_kapil@hotmail.com>
2025-07-13 14:27:17 +0530

    added static metadata

--- app/meals/page.js
+++ app/meals/page.js
@@ -4,6 +4,11 @@ import MealGrid from '@/components/meals/meals-grid'
 import { getMeals } from '@/lib/meals';
 import { Suspense } from 'react';
 
+export const metadata = {
+  title: 'All meals',
+  description: 'Browser the delicious meals shared by our vibrant community.'
+}
+
 async function Meals(){
   const meals = await getMeals();
   return <MealGrid meals={meals} />
